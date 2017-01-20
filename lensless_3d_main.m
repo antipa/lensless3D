1c87b55dbc9c5399fb5c414d6d59d7e033203bd4
antipa <naantipa@gmail.com>
2017-01-20 15:31:48 -0800

    random lenslets
    Updated random lenslets to be more physically accurate. It doesn't work
    yet due to sampling issues with Fresnel.

--- lensless_3d_main.m
+++ lensless_3d_main.m
@@ -1,16 +1,17 @@
 %Load impulse response stack, h
-h_in = load('../data/zstack_pco.mat');
-ht = (h_in.zstack);
-h = zeros(size(ht,1)/2,size(ht,2)/2,size(ht,3));
+h_in = load('C:\Users\herbtipa\Dropbox\3D data\h_substack_3d_color_pco.mat');
+ht = (h_in.hdouble);
+ds = 1/5;
+h = zeros(size(ht,1)*ds,size(ht,2)*ds,size(ht,3));
 for m = 1:size(h,3)
-    h(:,:,m) = imresize(ht(:,:,m),1/2,'box');
+    h(:,:,m) = imresize(ht(:,:,m),ds,'box');
 end
 clear ht;
 
 h = gpuArray(h);
 hn = norm(h(:,:,1),'fro');
 h = h./hn;
-z = h_in.z;
+%z = h_in.z;
 clear h_in
 
 %%
@@ -40,37 +41,42 @@ switch lower(meas_type)
         obj(250,400,20) = 1;
         b = A3d(obj);
     case 'measured'
-        bin = double(imread('C:\Users\herbtipa\Documents\MATLAB\robin_close.png'));
-        b = imresize(bin,1/4,'box');
+        im_dir = 'C:\Users\herbtipa\Dropbox\3D data\';
+        imfname = 'usaf_tilt_reverse.png';
+        bin = double(imread([im_dir,imfname]));
+        b = gpuArray(imresize(bin,1/2*ds,'box'));
 end
 
 % Define gradient handle
 GradErrHandle = @(x) linear_gradient_b(x,A3d,Aadj_3d,b);
 
 % Prox handle
-    tau = .001;
+    tau = .09;
 prox_handle = @(x)soft_nonneg(x,tau);
 
-
-options.stepsize = .000002;
+h1 = figure(1),clf
+options.fighandle = h1;
+options.stepsize = 5e-6;
 options.convTol = 8.2e-12;
 %options.xsize = [256,256];
-options.maxIter = 1000;
+options.maxIter = 3000;
 options.residTol = .2;
 options.momentum = 'nesterov';
 options.disp_figs = 1;
-options.disp_fig_interval = 20;   %display image this often
+options.disp_fig_interval = 30;   %display image this often
 options.xsize = size(h);
 nocrop = @(x)x;
 options.disp_crop = @(x)gather(real(sum(x,3)));
 options.disp_gamma = 1/2.2;
 options.known_input = 0;
 options.force_real = 1;
-init_style = 'xhat';
+init_style = 'zero';
 switch lower(init_style)
     case('zero')
         [xhat, funvals] = proxMin(GradErrHandle,prox_handle,gpuArray(zeros(size(h))),b,options);
     case('xhat')
         [xhat, funvals] = proxMin(GradErrHandle,prox_handle,xhat,b,options);
+    case('atb')
+        [xhat, funvals] = proxMin(GradErrHandle,prox_handle,Aadj_3d(b),b,options);
 end
 
