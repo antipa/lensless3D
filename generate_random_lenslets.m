329acaa02858431e53e1e2920cc6661650a6ce22
Nicholas Antipa <naantipa@gmail.com>
2017-01-19 17:59:43 -0800

    Gifs in random lenslets
    Now with more GIFS

--- generate_random_lenslets.m
+++ generate_random_lenslets.m
@@ -17,11 +17,11 @@ sph = @(x0,y0,R)sqrt(R^2-(X-x0).^2 - (Y-y0).^2);
 %%
 lenslet_surface = zeros(siz);
 for n = 1:length(lens_centers)
-zt = sph(x0(n,1),x0(n,2),R);
-lenslet_surface = max(real(zt),lenslet_surface);
-imagesc(lenslet_surface)
-axis image
-drawnow
+    zt = sph(x0(n,1),x0(n,2),R);
+    lenslet_surface = max(real(zt),lenslet_surface);
+    imagesc(lenslet_surface)
+    axis image
+    drawnow
 end
 
 %%
@@ -30,9 +30,30 @@ Ui = exp(-1i*2*pi*dn/lambda * lenslet_surface);
 fx = linspace(-1/2/px,1/2/px,size(Ui,2));
 fy = linspace(-1/2/px,1/2/px,size(Ui,1));
 [Fx, Fy] = meshgrid(fx,fy);
+
+h1 = figure(1);   %Make figure handle (h1 refers to figure 1, even if another figure is in focus)
+x = 0:2*pi;
+gif_out_filename = '../../random_lenslet_propagation_video.gif';   %Setup filename for gif
+
+
+
+n = 0;
 for Z = 1e-3:1000e-3:10
-U_out = propagate2(Ui,lambda,Z,Fx,Fy);
-I = abs(U_out).^2;
-imagesc(I), axis image
-drawnow
+    n = n+1;
+    U_out = propagate2(Ui,lambda,Z,Fx,Fy);
+    I = abs(U_out).^2;
+    imagesc(I), axis image
+    caxis([0 20])
+    drawnow
+    
+    frame = getframe(h1);   %Get data from figue 1
+    image_data = frame2im(frame);   %convert data to image information (this goes straight into avi)
+    [imind,cm] = rgb2ind(image_data,256);   %convert image information to index and colormap for gif (don't worry about this part)
+    
+%     if n == 1;   %Make sure this is the loop index == 1
+%         imwrite(imind,cm,gif_out_filename,'gif', 'Loopcount',inf);   %If you're on the first pass, make a new gif
+%     else
+%         imwrite(imind,cm,gif_out_filename,'gif','WriteMode','append');   %If it's not the first pass, append
+%     end
+    
 end
